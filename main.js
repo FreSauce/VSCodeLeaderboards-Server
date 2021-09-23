d3968a650323864f2df481734854096e5747edbd
Shardul Kurdukar <24shardul@gmail.com>
2021-09-23 22:31:19 +0530

    fixed bug

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-server.get("/", (req, res)=> {
+app.get("/", (req, res)=> {
   res.send({port: process.env.PORT})
 })
 
--- src/db.js
+++ src/db.js
@@ -1,5 +1,4 @@
 const client = require('./mongoConnect');
-require("dotenv").config();
 
 
 const addUser = async (data) => {
