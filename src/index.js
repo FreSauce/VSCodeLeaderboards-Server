696f90b5d705399835702fe6ca3335c6eccbe706
Shardul Kurdukar <24shardul@gmail.com>
2021-09-25 13:18:01 +0530

    paginated error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,40 @@ console.log(process.env.TOKEN);
 const client = new Discord.Client({ intents });
 const { getUsers, addUser, sendTick, getGlobalUsers } = require("./db");
 const { io } = require("../main");
+const { ButtonPaginator } = require("@psibean/discord.js-pagination");
+
+const paginated = (leaderboard, pageLength, isGlobal, messsage) => {
+    const pages = [];
+    const author = isGlobal
+        ? "Global Leaderboards"
+        : `Leaderboards for ${messsage.guild.name}`;
+    const iconURL = message.guild.iconURL({ dynamic: true });
+    const lblen = leaderboard.length;
+    const pageCount = Math.ceil(lblen / pageLength);
+    for (let i = 0; i < pageCount; i++) {
+        let userNames = "";
+        let time_spent = "";
+        for (
+            let j = pageLength * i;
+            j < min(pageLength, lblen - i * pageLength);
+            j++
+        ) {
+            const data = leaderboard[i * pageLength + j];
+            userNames += `\`${i * pageLength + j + 1}\` ${data.userName}\n`;
+            time_spent += ` \`${(data.activityTime / 60000).toFixed(2)}\`\n`;
+        }
+        const pageEmbed = new Discord.MessageEmbed()
+            .setAuthor(author, iconURL)
+            .setColor(0xe1abfb)
+            .addFields(
+                { name: "Top users", value: userNames, inline: true },
+                { name: "Minutes spent", value: time_spent, inline: true }
+            );
+        pages.push(pageEmbed);
+    }
+    return pages;
+};
+
 io.on("connection", (socket) => {
     socket.on("sendTick", (data) => {
         sendTick(data);
@@ -29,24 +63,9 @@ client.on("messageCreate", async (message) => {
         leaderboard.sort((a, b) => {
             return b.activityTime - a.activityTime;
         });
-        let userNames = "";
-        let time_spent = "";
-        for (let i = 0; i < leaderboard.length; i++) {
-            const data = leaderboard[i];
-            userNames += `\`${i + 1}\` ${data.userName}\n`;
-            time_spent += ` \`${(data.activityTime / 60000).toFixed(2)}\`\n`;
-        }
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(
-                `Leaderboard for ${message.guild.name}`,
-                message.guild.iconURL({ dynamic: true })
-            )
-            .setColor(0xe1abfb)
-            .addFields(
-                { name: "Top users", value: userNames, inline: true },
-                { name: "Minutes spent", value: time_spent, inline: true }
-            );
-        message.reply({ embeds: [embed] });
+        let pages = paginated(leaderboard, 10, false, message);
+        const buttonPaginator = new ButtonPaginator(interaction, { pages });
+        await buttonPaginator.send();
         return;
     }
 
@@ -55,24 +74,9 @@ client.on("messageCreate", async (message) => {
         leaderboard.sort((a, b) => {
             return b.activityTime - a.activityTime;
         });
-        let userNames = "";
-        let time_spent = "";
-        for (let i = 0; i < leaderboard.length; i++) {
-            const data = leaderboard[i];
-            userNames += `\`${i + 1}\` ${data.userName}\n`;
-            time_spent += ` \`${(data.activityTime / 60000).toFixed(2)}\`\n`;
-        }
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(
-                `Global Leaderboards`,
-                message.guild.iconURL({ dynamic: true })
-            )
-            .setColor(0xe1abfb)
-            .addFields(
-                { name: "Top users", value: userNames, inline: true },
-                { name: "Minutes spent", value: time_spent, inline: true }
-            );
-        message.reply({ embeds: [embed] });
+        let pages = paginated(leaderboard, 10, true, message);
+        const buttonPaginator = new ButtonPaginator(interaction, { pages });
+        await buttonPaginator.send();
         return;
     }
 });
