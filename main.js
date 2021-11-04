a35a9045407367fc3f8a8a96c9dcf0471bd842a2
fresauce <24shardul@gmail.com>
2021-11-04 09:39:27 +0530

    setup socket server

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000;
+const { addUser, sendTick } = require("./src/db");
 
-app.get("/", (req, res)=> {
-  res.send({port: process.env.PORT})
-})
-
-
-server.listen(process.env.PORT, () => {
-  console.log('listening on *:'+ process.env.PORT);
+io.on("connection", (socket) => {
+  socket.on("sendTick", (data) => {
+    sendTick(data);
+  });
+  socket.on("init", (data) => {
+    addUser(data);
+    console.log(data.username + " connected");
+  });
 });
 
-module.exports = {io};
+app.get("/", (req, res) => {
+  res.send({ port: port });
+});
 
+server.listen(port, () => {
+  console.log("listening on *:" + port);
+});
 
+module.exports = { io };
--- src/mongoConnect.js
+++ src/mongoConnect.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require("mongodb");
 const uri =
-    "mongodb+srv://fresauce:amogh123@vscodecluster.gltwn.mongodb.net/VSCodeCluster?retryWrites=true&w=majority";
+  "mongodb+srv://fresauce:amogh123@vscodecluster.gltwn.mongodb.net/VSCodeCluster?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-client.connect(err => console.log("connected"));
+client.connect((err) => console.log("Connected to MongoDB"));
 
 module.exports = client;
