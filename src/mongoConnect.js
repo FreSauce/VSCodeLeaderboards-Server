6b9bc360dde3d737ed0ddb9c24201932c2b21d6b
fresauce <24shardul@gmail.com>
2021-11-04 09:42:22 +0530

    fixed env vars

--- src/mongoConnect.js
+++ src/mongoConnect.js
@@ -1,11 +1,9 @@
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://fresauce:amogh123@vscodecluster.gltwn.mongodb.net/VSCodeCluster?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@vscodecluster.gltwn.mongodb.net/VSCodeCluster?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 client.connect((err) => console.log("Connected to MongoDB"));
-
 module.exports = client;
